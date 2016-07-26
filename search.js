b66cd9c335c1ecfc1b74357370bc7c4fcf7659d0
charlieperson <charlie.person@hyperisland.se>
2016-07-26 09:40:59 -0500

    Solves problem using non-mutative recursion

--- search.js
+++ search.js
@@ -1,7 +1,21 @@
+// for loop
+
 function findDivisible (divider, array) {
   var status = false;
   for(var i=0; i<array.length; i++){
     if(array[i] % divider === 0 && array[i] !== 0) status = true;
   }
   return status;
+}
+
+// recursion
+
+function findDivisible (divider, array) {
+  var i=0;
+  function findIt(num) {
+    if(array[i] % divider === 0 && array[i] !== 0) return true;
+    if(i >= array.length) return false;
+    return findIt(array[i+=1]);
+  }
+  return findIt(array[0]);
 }
